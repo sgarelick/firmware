593a26eb6f6558e6afab610edb79c6cd39427cf1
Connor Monahan <cmastudios@me.com>
2018-04-07 16:20:35 -0500

    Add shift register outputs

--- 2018/Dashboard/Dashboard.ino
+++ 2018/Dashboard/Dashboard.ino
@@ -1,11 +1,18 @@
+#include <SPI.h>
 #include <mcp_can.h>
+#include "shift.h"
 
+
+
+// pin for overheat light
+int overheatPin = A0;
 
 void setup() {
-  
+  pinMode(overheatPin, OUTPUT);
+  initShift();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  shiftUpdate();
 }
