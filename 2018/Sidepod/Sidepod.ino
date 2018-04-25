ea81b0cf4de08bfc7fc53ec0de307fd35f4904b0
Connor Monahan <cmastudios@me.com>
2018-04-25 17:52:24 -0500

    CAN logging SD version

--- 2018/Sidepod/Sidepod.ino
+++ 2018/Sidepod/Sidepod.ino
@@ -1,16 +1,20 @@
+#include <SPI.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include <SD.h>
 #include <Adafruit_GPS.h>
 #include <mcp_can.h>
 
 #include "CANBus18.h"
 #include "PacketSender.h"
 
+#define DEBUG true
+
 #define CAN0_INT 4
 MCP_CAN CAN0(10);
 
-const int GPS_RX = A1;
-const int GPS_TX = A0;
+const int GPS_RX = A3;
+const int GPS_TX = A4;
 
 SoftwareSerial gps(GPS_TX, GPS_RX);
 Adafruit_GPS GPS(&gps);
@@ -18,6 +22,9 @@ Adafruit_GPS GPS(&gps);
 const int rxPinXBee = A4;
 const int txPinXBee = A5;
 
+const bool sd_logging = true;
+const int sd_pin = 10;
+bool sd_working = false;
 
 SoftwareSerial XBee_Serial(rxPinXBee,txPinXBee);
 PacketSender XBee(XBee_Serial, 9600);
@@ -45,53 +52,59 @@ unsigned char len = 0;
 unsigned char rxBuf[8];
 
 void setup() {
-  //Serial.begin(115200);
-  // put your setup code here, to run once:
+  if (DEBUG)
+    Serial.begin(115200);
+    
   if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) {
-      //Serial.println("MCP2515 Initialized Successfully!");
-  }
-  else {
-      //Serial.println("Error Initializing MCP2515...");
+    if (DEBUG)
+      Serial.println(F("MCP2515 Initialized Successfully!"));
+  } else {
+    if (DEBUG)
+      Serial.println(F("Error Initializing MCP2515..."));
   }
   CAN0.setMode(MCP_NORMAL); // send acknowledgements to recieved data
   pinMode(CAN0_INT, INPUT);
   
   GPS.begin(9600);
   
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  // RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  
   // Set the update rate
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
   // Request updates on antenna status, comment out to keep quiet
   GPS.sendCommand(PGCMD_ANTENNA);
 
-  /*OCR0A = 0xAF;
-  TIMSK0 |= _BV(OCIE0A);*/
+  if (sd_logging) {
+    if (SD.begin(sd_pin)) {
+      sd_working = true;
+      if (DEBUG)
+        Serial.println(F("SD card initialized"));
+    } else {
+      if (DEBUG)
+        Serial.println(F("Failed to load SD card"));
+    }
+  }
 
   delay(500);
 }
 
 void loop() {
+  payload outgoing;
+  int rawRPM, rawLoad, rawThrottle, coolantC, rawIntake, rawo2, rawSpeed, rawVolts, rawIgn, rawRB, rawFB;
+  float rpm, load, throttle, coolantF, intake, o2, vehicleSpeed, volts, ign, rearBrake, frontBrake;
+  byte gear;
   if (GPS.newNMEAreceived()) {
     // a tricky thing here is if we print the NMEA sentence, or data
     // we end up not listening and catching other sentences! 
     // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
     //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-    payload outgoing;
     if (!GPS.parse(GPS.lastNMEA())) {
       outgoing.intData = 0;
       XBee.sendPayloadTimestamp(outgoing, 0x42);
       outgoing.intData = 0;
       XBee.sendPayloadTimestamp(outgoing, 0x43);
     } else {
+      log_gps_data();
       outgoing.intData = GPS.latitude_fixed;
       XBee.sendPayloadTimestamp(outgoing, 0x42);
       outgoing.intData = GPS.longitude_fixed;
@@ -102,43 +115,40 @@ void loop() {
   if (!digitalRead(CAN0_INT)) {
     //Serial.println("got data");
     CAN0.readMsgBuf(&rxId, &len, rxBuf);
-
-      
-      payload outgoing;
-
+    log_can_data();
       switch (rxId & 0x1FFFFFFF) {
 
         case ID_AEMEngine0: {
 
-            int rawRPM = GET_AEMEngine0_EngineSpeed(rxBuf);
-            float rpm = CALC_AEMEngine0_EngineSpeed(rawRPM, 1.0);
+            rawRPM = GET_AEMEngine0_EngineSpeed(rxBuf);
+            rpm = CALC_AEMEngine0_EngineSpeed(rawRPM, 1.0);
 
             outgoing.floatData = rpm;
             XBee.sendPayloadTimestamp(outgoing, 0x30);
 
-            int rawLoad = GET_AEMEngine0_EngineLoad(rxBuf);
-            float load = CALC_AEMEngine0_EngineLoad(rawLoad, 1.0);
+            rawLoad = GET_AEMEngine0_EngineLoad(rxBuf);
+            load = CALC_AEMEngine0_EngineLoad(rawLoad, 1.0);
 
             outgoing.floatData = load;
             XBee.sendPayloadTimestamp(outgoing, 0x31);
             //Display.sendPayload(outgoing, 0x31);
 
             //log throttle position
-            int rawThrottle = GET_AEMEngine0_ThrottlePos(rxBuf);
-            float throttle = CALC_AEMEngine0_ThrottlePos(rawThrottle, 1.0);
+            rawThrottle = GET_AEMEngine0_ThrottlePos(rxBuf);
+            throttle = CALC_AEMEngine0_ThrottlePos(rawThrottle, 1.0);
 
             outgoing.floatData = throttle;
             XBee.sendPayloadTimestamp(outgoing, 0x32);
 
             //log coolant temp
-            int coolantC = GET_AEMEngine0_CoolantTemp(rxBuf);
-            float coolantF = CALC_AEMEngine0_CoolantTemp(coolantC, 1.0);
+            coolantC = GET_AEMEngine0_CoolantTemp(rxBuf);
+            coolantF = CALC_AEMEngine0_CoolantTemp(coolantC, 1.0);
 
             outgoing.floatData = coolantF;
             XBee.sendPayloadTimestamp(outgoing, 0x33);
 
-            int rawIntake = GET_AEMEngine0_IntakeManifoldAirTemp(rxBuf);
-            float intake = CALC_AEMEngine0_IntakeManifoldAirTemp(rawIntake, 1.0);
+            rawIntake = GET_AEMEngine0_IntakeManifoldAirTemp(rxBuf);
+            intake = CALC_AEMEngine0_IntakeManifoldAirTemp(rawIntake, 1.0);
 
             outgoing.floatData = intake;
             XBee.sendPayloadTimestamp(outgoing, 0x44);
@@ -153,8 +163,8 @@ void loop() {
             o2 = rawo2 * O2_SCALE + 0.5;*/
             //dataLine = dataLine + o2 +  ", ";
 
-            int rawo2 = GET_AEMEngine3_AFR1(rxBuf);
-            float o2 = CALC_AEMEngine3_AFR1(rawo2, 1.0);
+            rawo2 = GET_AEMEngine3_AFR1(rxBuf);
+            o2 = CALC_AEMEngine3_AFR1(rawo2, 1.0);
 
             outgoing.floatData = o2;
             XBee.sendPayloadTimestamp(outgoing, 0x34);
@@ -164,25 +174,25 @@ void loop() {
             vehicleSpeed = rawSpeed * SPEED_SCALE;*/
             //dataLine = dataLine + vehicleSpeed + ", ";
 
-            int rawSpeed = GET_AEMEngine3_VehicleSpeed(rxBuf);
-            float vehicleSpeed = CALC_AEMEngine3_VehicleSpeed(rawSpeed, 1.0);
+            rawSpeed = GET_AEMEngine3_VehicleSpeed(rxBuf);
+            vehicleSpeed = CALC_AEMEngine3_VehicleSpeed(rawSpeed, 1.0);
 
             outgoing.floatData = vehicleSpeed;
             XBee.sendPayloadTimestamp(outgoing, 0x35);
             //Display.sendPayload(outgoing, 0x35);
 
-            byte gear = rxBuf[4];
+            gear = rxBuf[4];
             XBee.sendByteTimestamp(gear, 0x36);
             //Display.sendByte(gear, 0x36);
 
-            int rawVolts = GET_AEMEngine3_ECUBatteryVoltage(rxBuf);
-            float volts = CALC_AEMEngine3_ECUBatteryVoltage(rawVolts, 1.0);
+            rawVolts = GET_AEMEngine3_ECUBatteryVoltage(rxBuf);
+            volts = CALC_AEMEngine3_ECUBatteryVoltage(rawVolts, 1.0);
 
             outgoing.floatData = volts;
             XBee.sendPayloadTimestamp(outgoing, 0x37);
 
-            int rawIgn = GET_AEMEngine3_IgnitionTiming(rxBuf);
-            float ign = CALC_AEMEngine3_IgnitionTiming(rawIgn, 1.0);
+            rawIgn = GET_AEMEngine3_IgnitionTiming(rxBuf);
+            ign = CALC_AEMEngine3_IgnitionTiming(rawIgn, 1.0);
 
             outgoing.floatData = ign;
             XBee.sendPayloadTimestamp(outgoing, 0x45);
@@ -195,13 +205,13 @@ void loop() {
           }
 
         case ID_RearController0: {
-            int rawRB = GET_RearController0_RearBrakePressure(rxBuf);
-            float rearBrake = CALC_RearController0_RearBrakePressure(rawRB, 1.0);
+            rawRB = GET_RearController0_RearBrakePressure(rxBuf);
+            rearBrake = CALC_RearController0_RearBrakePressure(rawRB, 1.0);
             outgoing.floatData = rearBrake;
             XBee.sendPayloadTimestamp(outgoing, 0x41);
 
-            int rawFB = GET_RearController0_FrontBrakePressure(rxBuf);
-            float frontBrake = CALC_RearController0_FrontBrakePressure(rawFB, 1.0);
+            rawFB = GET_RearController0_FrontBrakePressure(rxBuf);
+            frontBrake = CALC_RearController0_FrontBrakePressure(rawFB, 1.0);
             outgoing.floatData = frontBrake;
             XBee.sendPayloadTimestamp(outgoing, 0x40);
 
@@ -220,3 +230,47 @@ void loop() {
       }
   }
 }
+
+char timestr[20];
+void update_time() {
+  sprintf(timestr, "%04d-%02d-%02d-%02d-%02d-%02d", GPS.year, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds);
+}
+
+void log_can_data()
+{
+  if (!sd_working)
+    return;
+  int i;
+  File file = SD.open("can.txt", FILE_WRITE);
+  if (file) {
+    file.println();
+    file.println("FSAE");
+    update_time();
+    file.println(timestr);
+    file.println(rxId);
+    file.println(len);
+    for (i = 0; i < len; i++) {
+      file.write(rxBuf[i]);
+    }
+    file.close();
+  }
+}
+
+void log_gps_data()
+{
+  if (!sd_working)
+    return;
+  int i;
+  File file = SD.open("gps.txt", FILE_WRITE);
+  if (file) {
+    update_time();
+    file.print(timestr);
+    file.print(",");
+    file.print(GPS.latitude);
+    file.print(",");
+    file.print(GPS.longitude);
+    file.println();
+    file.close();
+  }
+}
+
