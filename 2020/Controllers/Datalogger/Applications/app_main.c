4467671d970e8bbbdee5d65f00728b9668de9c77
Connor Monahan <cma2714@gmail.com>
2020-10-02 10:55:55 -0500

    Add project for datalogger

---
+++ 2020/Controllers/Datalogger/Applications/app_main.c
@@ -0,0 +1,11 @@
+#include "app_main.h"
+
+void app_init(void)
+{
+	
+}
+
+void app_periodic(void)
+{
+	
+}
---
+++ 2020/Controllers/Datalogger/Drivers/drv_main.c
@@ -0,0 +1,11 @@
+#include "drv_main.h"
+
+void drv_init(void)
+{
+	
+}
+
+void drv_periodic(void)
+{
+	
+}
