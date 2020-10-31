7adfe19d3c38d91e427f24afd33e3cf9853bbc3a
Connor Monahan <cma2714@gmail.com>
2020-10-31 14:31:04 -0500

    Tested working LEDs, CA fix

--- 2020/Controllers/Dashboard/Applications/sevenseg.c
+++ 2020/Controllers/Dashboard/Applications/sevenseg.c
@@ -25,7 +25,7 @@ const uint8_t DisplayAddresses[5] = {
 void sevenseg_init(void) {
     const uint8_t tx[] = {0x00, 0x00};
     for(uint8_t address = 0x25; address < 0x28; address++) {
-        drv_i2c_write_register(DRV_I2C_CHANNEL_EXPANDERS, address, 0x00, tx, 2);
+        while (drv_i2c_write_register(DRV_I2C_CHANNEL_EXPANDERS, address, 0x00, tx, 2) != 2) {};
     }
 }
 
@@ -36,7 +36,7 @@ void set_digit(uint8_t display_index, uint8_t digit) {
         return; 
     }
     
-    uint8_t code = SevenSegmentDigits[digit];
+    uint8_t code = ~SevenSegmentDigits[digit];
     uint8_t address = DisplayAddresses[display_index];
     uint8_t tx = code;
     
