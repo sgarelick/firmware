6306d70a2ddfa345524e498b05d5990d60197811
bsiderowf <bsiderowf@gmail.com>
2020-10-31 13:15:23 -0400

    initial sevenseg pass

---
+++ 2020/Controllers/Dashboard/Applications/sevenseg.c
@@ -0,0 +1,53 @@
+#include "drv_i2c.h"
+#include "sevenseg.h"
+
+const uint8_t SevenSegmentDigits[10] = {
+	0x3F, /* 0 */
+	0x06, /* 1 */
+	0x5B, /* 2 */
+	0x4F, /* 3 */
+	0x66, /* 4 */
+	0x6D, /* 5 */
+	0x7D, /* 6 */
+	0x07, /* 7 */
+	0x7F, /* 8 */
+	0x6F, /* 9 */
+};
+
+const uint8_t DisplayAddresses[5] = {
+    0x25,
+    0x26,
+    0x26,
+    0x27,
+    0x27
+};
+
+void set_digit(uint8_t display_index, uint8_t digit) {
+    if(digit > 9) {
+        // Invalid digit
+        return; 
+    }
+    
+    uint8_t code = SevenSegmentDigits[digit];
+    uint8_t address = DisplayAddresses[display_index];
+    const uint8_t tx[] = {code, 0};
+    drv_i2c_write_register(DRV_I2C_CHANNEL_EXPANDERS, address, 0x12, tx, 2);
+}
+
+void set_rpm(uint16_t rpm) {
+    if(rpm > 10000) {
+        // invalid rpm
+        return;
+    }
+    
+    // Write the last four displays
+    for(int i = 1; i < 5; i++) {
+        int digit = rpm % 10;
+        rpm /= 10;
+        set_digit(i, digit);
+    }
+}
+
+void set_gear(uint8_t gear) {
+    set_digit(0, gear);
+}
---
+++ 2020/Controllers/Dashboard/Applications/sevenseg.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   sevenseg.h
+ * Author: bsiderowf
+ *
+ * Created on October 31, 2020, 12:56 PM
+ */
+
+#ifndef SEVENSEG_H
+#define	SEVENSEG_H
+
+void set_digit(uint8_t display_index, uint8_t digit);
+void set_rpm(uint16_t rpm);
+void set_gear(uint8_t gear);
+
+#endif	/* SEVENSEG_H */
+
