e641de54f9171e449d6ab1e463952ade86e6812f
bsiderowf <bsiderowf@gmail.com>
2020-11-21 13:59:28 -0500

    Recieve coolant temp from CAN

--- 2020/Controllers/Dashboard/Applications/app_main.c
+++ 2020/Controllers/Dashboard/Applications/app_main.c
@@ -4,6 +4,9 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "drv_can.h"
+#include "drv_can_private.h"
+
 volatile int GearPosition, EngineSpeed;
 volatile int failures;
 
@@ -22,7 +25,7 @@ void app_periodic(void)
 	
     set_gear(failures);
     //set_rpm(EngineSpeed);
-   set_rgb_one_digit(GearPosition, RGB_MAGENTA);
+    set_rgb_one_digit(GearPosition, RGB_MAGENTA);
     
 	unsigned time = xTaskGetTickCount();
 	if (time % 1000 == 0)
@@ -35,5 +38,15 @@ void app_periodic(void)
 	
 //    set_gear(GearPosition);
     //set_rpm(EngineSpeed);
+    
+    struct drv_can_rx_buffer_element* element = drv_can_get_rx_buffer(DRV_CAN_RX_BUFFER_CAN0_PE6);
+    uint16_t coolant_temp_raw = element->DB[4] | (element->DB[5] << 8);
+    float coolant_temp = coolant_temp_raw * 0.1;
+    
+    if(coolant_temp > 200) {
+        set_gear(1);
+    } else {
+        set_gear(0);
+    }
     
 }
